dfb3c5413e2a74b3110dfbff230dc5fbdfb7ddcf
gcdadich <97729024+gcdadich@users.noreply.github.com>
2024-03-11 23:21:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String input[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(input[0]);
+            int x = Integer.parseInt(input[1]);
+            
+            int mat1[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat1[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+            int mat2[][] = new int[n][n];
+            
+            for(int i = 0;i<n;i++){
+                input = read.readLine().split(" ");
+                for(int j = 0;j<n;j++){
+                    mat2[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+            
+        
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countPairs(mat1,mat2,n,x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int countPairs(int mat1[][], int mat2[][], int n, int x) {
+        // code here
+        int count = 0,i = 0,j = 0,a=n-1,b=n-1;
+        while(i<n && j<n && a>=0 && b>=0){
+            int sum = mat1[i][j]+mat2[a][b];
+            if(sum==x){
+                count++;
+                j++;
+                b--;
+            }else if(sum<x){
+                j++;
+            }else
+               b--;
+               if(b<0){
+                   b=n-1;
+                   a--;
+               }
+               if(j>=n){
+                   j=0;
+                   i++;
+               }
+        }
+        return count;
+    }
+}
