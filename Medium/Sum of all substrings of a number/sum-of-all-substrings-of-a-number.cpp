a1bb81a7b3edba2a18f85aa7931b58235dc6990f
gcdadich <97729024+gcdadich@users.noreply.github.com>
2024-04-04 23:34:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find sum of all possible substrings of the given string.
+    long long sumSubstrings(string s)
+{
+    long long ans = 0, previous = 0, mod = 1e9 + 7;
+    for (int i = 0; i < s.length(); i++)
+    {
+        long long curr = (previous * 10) % mod + (s[i] - '0') * (i + 1) % mod;
+        previous = curr;
+        ans = (ans + curr) % mod;
+    }
+    return ans;
+}
+
+int main()
+{
+    string s = "1234";
+    cout<<sumSubstrings(s);
+    return 0;
+}
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking string
+        string s;
+        cin>>s;
+        Solution ob;
+        //calling sumSubstrings() function
+        cout << ob.sumSubstrings(s) << endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
